b1918c400cdfa2c47f23297768d26f1d987cb4a1
nawinsharma <nawinsharma60@gmail.com>
2025-08-26 11:01:17 +0530

    Refactor Prisma client initialization for improved global access
    - Introduced a singleton pattern for the Prisma client to ensure a single instance is used across the application.
    - Added global variable handling to maintain the Prisma client instance in non-production environments.
    - Updated export to accommodate the new singleton approach.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,14 @@
 import { PrismaClient } from "@/generated/prisma";
+const prismaClientSingleton = () => {
+   return new PrismaClient();
+};
 
-const prisma = new PrismaClient();
+declare const globalThis: {
+   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 export default prisma;
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
